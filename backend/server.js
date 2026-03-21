08593da7a1a211432b6e16b9bcb3323d3ea9c4d9
z0mart <zs.marton29@gmail.com>
2026-03-21 11:29:56 +0100

    ff
    dfdfd

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// --- ÚJ RÉSZ: Útvonalak beimportálása ---
+const pilotRoutes = require('./routes/pilotRoutes');
+app.use('/api/pilots', pilotRoutes);
+// ---------------------------------------
+
 const PORT = process.env.PORT || 5000;
 
-// Adatbázis szinkronizáció
-// A 'force: false' nem töröl! csak létrehozza a táblát ha még nincs 
 db.sequelize.sync({ force: false })
     .then(() => {
-        console.log('Siker: Kapcsolódva a MariaDB-hez és a táblák szinkronizálva.');
+        console.log('Siker: Kapcsolódva a MariaDB-hez.');
         app.listen(PORT, () => {
             console.log(`A szerver fut a http://localhost:${PORT} címen.`);
         });
     })
     .catch(err => {
-        console.error('Hiba történt az adatbázis szinkronizációjakor:', err);
+        console.error('Hiba:', err);
     });
