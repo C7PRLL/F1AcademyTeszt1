f3812e21dd28d8c71c4eafc9274ce588c9ce8e76
z0mart <zs.marton29@gmail.com>
2026-03-21 14:33:37 +0100

    dd
    dsds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Pilots from './pages/Pilots'; 
-import Register from './pages/Register'; // 1. EZT ADD HOZZÁ AZ IMPORTOKHOZ!
+import Register from './pages/Register';
+import Login from './pages/Login'; // 1. ÚJ IMPORT A BEJELENTKEZÉSHEZ
 import './App.css'; 
 
 function App() {
@@ -14,8 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pilots" element={<Pilots />} />
-          {/* 2. IDE KERÜL AZ ÚJ ROUTE: */}
           <Route path="/register" element={<Register />} /> 
+          
+          {/* 2. ÚJ ÚTVONAL A BEJELENTKEZÉSHEZ: */}
+          <Route path="/login" element={<Login />} /> 
           
           {/* Később ide jöhet majd a: <Route path="/diagrams" element={<Diagrams />} /> */}
         </Routes>
